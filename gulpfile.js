da037b74ba7fc7f1c1e49607fa02afcd007425d1
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2016-07-16 00:28:02 -0300

    add typescript source and webpack!

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
-var elixir = require('laravel-elixir');
+process.env.DISABLE_NOTIFIER = true;
+
+var elixir = require('laravel-elixir'),
+  path = require('path'),
+  webpack = require('webpack');
+
+require('laravel-elixir-livereload');
+require('laravel-elixir-webpack-ex');
 
 /*
  |--------------------------------------------------------------------------
@@ -12,5 +19,84 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+  /**
+   * Bootstrap
+   **/
+  var bootstrapPath = 'node_modules/bootstrap-less';
+
+  mix.copy(bootstrapPath + '/bootstrap', 'resources/assets/less');
+  mix.copy(bootstrapPath + '/fonts', 'public/fonts');
+  mix.copy(bootstrapPath + '/js', 'public/js');
+
+  /**
+   * JQuery
+   **/
+  mix.copy('node_modules/jquery/dist/jquery.min.js', 'public/js/jquery');
+
+  /**
+   * Less
+   **/
+  mix.less('bootstrap.less');
+
+  /**
+   * Scripts webpack bundling and copying
+   **/
+
+  mix.webpack({
+    vendor: 'vendor.ts',
+    app: 'app.ts'
+  }, {
+    debug: true,
+    devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.ts$/,
+          loader: 'awesome-typescript-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        '__decorate': 'typescript-decorate',
+        '__extends': 'typescript-extends',
+        '__param': 'typescript-param',
+        '__metadata': 'typescript-metadata'
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js',
+        minChunks: Infinity
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'app',
+        filename: 'app.js',
+        minChunks: 4,
+        chunks: [
+          'app'
+        ]
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        minimize: true,
+        mangle: false
+      })
+    ]
+  }, 'public/js', 'resources/assets/typescript');
+
+  /**
+   * LiveReload
+   **/
+  mix.livereload([
+    'resources/assets/typescript/**/**/**/**/*',
+    'public/css/**/*',
+    'public/fonts/**/*',
+    'public/js/**/*'
+  ]);
 });
